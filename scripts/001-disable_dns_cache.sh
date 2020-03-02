14e72c634396ca5c4a6b4a348493475a71ede22b
Dan Charousek <DanCharousek@gmail.com>
2020-03-02 12:37:21 +0100

    Fix fpm port and host substitution

--- scripts/001-disable_dns_cache.sh
+++ scripts/001-disable_dns_cache.sh
@@ -2,14 +2,14 @@
 
 REPLACE=""
 
+[ -z $FPM_HOST ] && FPM_HOST="php-fpm"
+[ -z $FPM_PORT ] && FPM_PORT="9000"
+
 if [ ! -z $DISABLE_DNS_CACHE ] && [ $DISABLE_DNS_CACHE="1" ]; then
     if [ -z $RESOLVER ]; then
         echo "You must provide resolver in order to disable DNS cache"
         exit 1
     fi
-
-    [ -z $FPM_HOST ] && FPM_HOST="php-fpm"
-    [ -z $FPM_PORT ] && FPM_PORT="9000"
 
     echo "Disabling DNS cache"
     REPLACE="resolver $RESOLVER valid=5s ipv6=off;\n            set \$fpm_upstream \"$FPM_HOST:$FPM_PORT\";\n            fastcgi_pass   \$fpm_upstream;"
