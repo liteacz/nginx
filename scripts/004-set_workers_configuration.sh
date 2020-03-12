fee7285d357afb314b24756634fecac0501109e6
Dan Charousek <DanCharousek@gmail.com>
2020-03-12 23:37:38 +0100

    Calculate default value for worker connections automatically

--- scripts/004-set_workers_configuration.sh
+++ scripts/004-set_workers_configuration.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+cores=$(nproc --all)
+defaultConnections="4096"
+
+if [ ! -z $cores ]; then
+    defaultConnections=$((cores * 1024))
+fi
+
 [ -z $WORKER_PROCESSES ] && WORKER_PROCESSES="auto"
-[ -z $WORKER_CONNECTIONS ] && WORKER_CONNECTIONS="51200"
-[ -z $WORKER_RLIMIT_NO_FILE ] && WORKER_RLIMIT_NO_FILE="51200"
+[ -z $WORKER_CONNECTIONS ] && WORKER_CONNECTIONS="$defaultConnections"
+[ -z $WORKER_RLIMIT_NO_FILE ] && WORKER_RLIMIT_NO_FILE="$defaultConnections"
 
 sed -i -e "
 s~\$WORKER_PROCESSES~$WORKER_PROCESSES~g
