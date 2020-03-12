f2212fac1028c7da9229f9db031e7780e3bacb9a
Dan Charousek <DanCharousek@gmail.com>
2020-03-12 11:09:32 +0100

    Use delimiter not coliding with dir separator

--- scripts/003-set_errors_dir.sh
+++ scripts/003-set_errors_dir.sh
@@ -3,4 +3,4 @@
 [ -z $HTTP_ERRORS_DIR ] && HTTP_ERRORS_DIR="/var/www/errors"
 
 mkdir -p $HTTP_ERRORS_DIR
-sed -i -e "s/\$HTTP_ERRORS_DIR/$HTTP_ERRORS_DIR/g" /etc/nginx/nginx.conf
+sed -i -e "s~\$HTTP_ERRORS_DIR~$HTTP_ERRORS_DIR~g" /etc/nginx/nginx.conf
